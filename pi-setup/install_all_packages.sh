59e9fbdac2f1d240568b3a99479e0d1cb896aab8
frank26080115 <frank.zhao.main@gmail.com>
2022-05-24 00:51:44 -0700

    Update install_all_packages.sh

--- pi-setup/install_all_packages.sh
+++ pi-setup/install_all_packages.sh
@@ -2,14 +2,17 @@
 
 echo "starting to install all packages required for Bucket"
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 sudo apt-get -y install python3-pip
 pip3 install --upgrade setuptools
+sudo apt-get -y install debhelper build-essential git
+sudo apt-get -y install cpufrequtils
 
 sudo apt-get -y install python-rpi.gpio python3-rpi.gpio
 sudo apt-get -y install python3-gpiozero
+sudo apt-get -y install i2c-tools libgpiod-dev
 sudo apt-get -y install python3-smbus
 sudo apt-get -y install python3-pil
 sudo apt-get -y install python3-psutil
@@ -17,11 +20,14 @@ sudo apt-get -y install python3-pyftpdlib
 pip3 install adafruit-circuitpython-ssd1306
 pip3 install pathlib
 
-echo "calling Adafruit's Blinka installer"
+sudo apt-get -y install exfat-fuse exfat-utils
+# the following might fail
+#sudo apt-get -y install usbmount
 
 pip3 install --upgrade adafruit-python-shell
-sudo pip3 install --upgrade adafruit-python-shell
-wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
-sudo python3 raspi-blinka.py
+pip3 install --upgrade RPi.GPIO
+pip3 install --upgrade adafruit-blinka
 
 echo "finished all package installation tasks"
+
+echo "TODO: install usbmount"
